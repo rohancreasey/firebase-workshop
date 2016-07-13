9543a06f2fd64709990dd0783d2d660980abd98d
Rohan Creasey <rcreasey@rubiconproject.com>
2016-07-13 18:36:02 +1000

    added Firebase initialisation rules

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 // TODO: Paste the Firebase initialization code first
 
+<script src="https://www.gstatic.com/firebasejs/3.2.0/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyC24_yyeBozILcR6iruKVkegLfvffE15yU",
+    authDomain: "rohancreasey-firebase-workshop.firebaseapp.com",
+    databaseURL: "https://rohancreasey-firebase-workshop.firebaseio.com",
+    storageBucket: "",
+  };
+  firebase.initializeApp(config);
+</script>
+    
 // TODO: Your code below
 
